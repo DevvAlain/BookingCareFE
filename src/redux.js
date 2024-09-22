2563eb9bd8d5116ff40ba54922e36961bbbc7770
Alain <vuthanhduc181203@gmail.com>
2024-09-22 20:54:06 +0700

    qua  met moi

--- src/redux.js
+++ src/redux.js
@@ -10,7 +10,7 @@ import { persistStore } from 'redux-persist';
 import createRootReducer from './store/reducers/rootReducer';
 import actionTypes from './store/actions/actionTypes';
 
-const environment = process.env.NODE_ENV || "development";
+const environment = process.env.REACT_APP_NODE_ENV || "development";
 let isDevelopment = environment === "development";
 
 //hide redux logs
